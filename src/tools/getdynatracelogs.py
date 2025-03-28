80c21402c66a31295dd2f931fe37fd1d1cc6c63e
deividfoggi <deividfoggi@hotmail.com>
2025-03-28 14:21:40 -0300

    Load environment variables for Dynatrace API; refactor get_dynatrace_logs function to use global HttpClient instance

--- src/tools/getdynatracelogs.py
+++ src/tools/getdynatracelogs.py
@@ -1,16 +1,20 @@
+import os
+from dotenv import load_dotenv
 from utils.httpclient import HttpClient
 from autogen_core import CancellationToken
 from autogen_core.tools import FunctionTool
 from typing_extensions import Annotated
 import asyncio
 
+load_dotenv()
+
 api_url = os.getenv("DYNA_TRACE_API_URL")
 api_key = os.getenv("DYNA_TRACE_API_KEY")
 http_client = HttpClient(api_url, api_key)
 
-async def get_dynatrace_logs(self, path: str, query: str):
+async def get_dynatrace_logs(path: str, query: str):
     # get logs using the HttpClient
-    response = self.http_client.get(f"{path}{query}")
+    response = http_client.get(f"{path}{query}")
     return response
 
 get_dynatracelogs_tool = FunctionTool(
