de2b48a9d6da51e62584aa4d931ec892455d4a72
deividfoggi <deividfoggi@hotmail.com>
2025-03-31 20:05:00 -0300

    Add Azure Monitor query functionality and token retrieval methods

---
+++ src/tools/queryazmonitor.py
@@ -0,0 +1,66 @@
+from azure.identity import DefaultAzureCredential, ClientSecretCredential
+from azure.monitor.query import LogsQueryClient, TokenCredential
+from datetime import datetime, timedelta
+import json
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def query_azure_monitor(resource_id: str, query: str, time_span: timedelta):
+    """
+    Executa uma query em um recurso no Azure Monitor workspace em busca de logs.
+
+    Parâmetros:
+    - resource_id (str): ID do recurso a ser consultado.
+    - query (str): Query Kusto a ser executada.
+    - time_span (timedelta): Intervalo de tempo para a consulta.
+    Retorno:
+    - json: Saída do comando ou erro.
+    """
+    credential = DefaultAzureCredential()
+    client = LogsQueryClient(credential)
+    #workspace_id = "/subscriptions/00ec1d2c-df0a-49cb-bb6c-848f54417bf5/resourceGroups/MAS-AKS-MVP/providers/microsoft.monitor/accounts/AMW-AKSMonitoring-001"
+    results = []
+
+    try:
+        response = client.query_resource(
+            resource_id=resource_id,
+            query=query,
+            timespan=time_span,
+        )
+
+        if response.tables:
+            for table in response.tables:
+                for row in table.rows:
+                    row_dict = dict(zip(table.columns_names, row))
+                    results.append(row_dict)
+
+        return json.dumps({"status": "success", "logs": results})
+
+    except Exception as e:
+        return json.dumps({"status": "error", "message": str(e)})
+    
+def get_token(UseDefaultCredential: bool = True):
+    """
+    Obtém um token de acesso para autenticação no Azure.
+
+    Parâmetros:
+    - UseDefaultCredential (bool): Se True, utiliza as credenciais padrão do Azure.
+
+    Retorno:
+    - str: Token de acesso.
+    """
+    if UseDefaultCredential:
+        credential = DefaultAzureCredential()
+        token = credential.get_token("https://management.azure.com/.default")
+        return token.token
+    else:
+        credential = ClientSecretCredential(
+            tenant_id=os.getenv("AZURE_TENANT_ID"),
+            client_id=os.getenv("AZURE_CLIENT_ID"),
+            client_secret=os.getenv("AZURE_CLIENT_SECRET"),
+        )
+        scope =  "https://management.azure.com/.default"
+        token = credential.get_token(scope)
+        return token.access_token
