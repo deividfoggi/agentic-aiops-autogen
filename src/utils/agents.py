830ccef59a841fe1f7485c5362be4ccf33bde7c9
Deivid de Foggi <dfoggi@microsoft.com>
2025-04-03 23:25:26 +0000

    Updated agents.py

--- src/utils/agents.py
+++ src/utils/agents.py
@@ -3,7 +3,7 @@ from autogen_ext.models.openai import AzureOpenAIChatCompletionClient
 from autogen_agentchat.agents import AssistantAgent
 from autogen_agentchat.teams import MagenticOneGroupChat
 from autogen_agentchat.ui import Console
-#from tools.getdynatracelogs import get_dynatrace_logs
+from tools.getdynatracelogs import get_dynatrace_logs
 from tools.shell import shell
 from tools.queryazmonitor import query_azure_monitor
 from utils.config import Config
@@ -15,24 +15,24 @@ load_dotenv()
 class Agents:
     def __init__(self):
 
-        token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+        #token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
 
         self.az_model_client = AzureOpenAIChatCompletionClient(
             azure_deployment=Config.aoai_deployment,
             model=Config.aoai_model,
             api_version=Config.aoai_version,
             azure_endpoint=Config.aoai_endpoint,
-            api_key=Config.aoai_api_key,
-            azure_ad_token_provider=token_provider
+            api_key=Config.aoai_api_key
+            #azure_ad_token_provider=token_provider
         )
 
         #Comment out this agent to prevent it from being used during tests with Azure Monitor
-        # self.dynatrace_specialist = AssistantAgent(
-        #     name="Assistant",
-        #     model_client=self.az_model_client,
-        #     system_message=get_prompt("dynatrace_specialist"),
-        #     tools=[get_dynatrace_logs]
-        # )
+        self.dynatrace_specialist = AssistantAgent(
+            name="dynatrace_specialist",
+            model_client=self.az_model_client,
+            system_message=get_prompt("dynatrace_specialist"),
+            tools=[get_dynatrace_logs]
+        )
 
         self.aks_specialist = AssistantAgent(
             name="aks_specialist",
@@ -48,7 +48,7 @@ class Agents:
             tools=[query_azure_monitor]
         )
 
-        self.team = MagenticOneGroupChat([self.aks_specialist, self.kql_specialist], model_client=self.az_model_client)
+        self.team = MagenticOneGroupChat([self.aks_specialist, self.dynatrace_specialist], model_client=self.az_model_client)
     
     async def run_task(self, event:str):
         """
