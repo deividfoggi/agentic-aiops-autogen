7c967032a11c5790603a41ab0cfa10aa0d9da90d
deividfoggi <deividfoggi@hotmail.com>
2025-04-03 20:30:27 -0300

    Refactor agents.py to conditionally initialize AzureOpenAIChatCompletionClient based on environment

--- src/utils/agents.py
+++ src/utils/agents.py
@@ -15,16 +15,23 @@ load_dotenv()
 class Agents:
     def __init__(self):
 
-        #token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
-
-        self.az_model_client = AzureOpenAIChatCompletionClient(
+        if Config.environment == "dev":
+            self.az_model_client = AzureOpenAIChatCompletionClient(
             azure_deployment=Config.aoai_deployment,
             model=Config.aoai_model,
             api_version=Config.aoai_version,
             azure_endpoint=Config.aoai_endpoint,
             api_key=Config.aoai_api_key
-            #azure_ad_token_provider=token_provider
-        )
+            )
+        else:
+            token_provider = get_bearer_token_provider(DefaultAzureCredential(), "https://cognitiveservices.azure.com/.default")
+            self.az_model_client = AzureOpenAIChatCompletionClient(
+            azure_deployment=Config.aoai_deployment,
+            model=Config.aoai_model,
+            api_version=Config.aoai_version,
+            azure_endpoint=Config.aoai_endpoint,
+            azure_ad_token_provider=token_provider
+            )
 
         self.dynatrace_specialist = AssistantAgent(
             name="dynatrace_specialist",
